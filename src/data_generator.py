d27e8ccd29f987a820d9aa99ac30641542fc4330
Siddharth Manne <sidmanne13@gmail.com>
2025-08-03 11:30:35 +0530

    Initial project structure

---
+++ src/data_generator.py
@@ -0,0 +1,10 @@
+class SpacecraftTelemetryGenerator:
+    """
+    Generates realistic spacecraft telemetry with controllable failure modes
+    
+    Key insight: Real spacecraft data has:
+    - Normal operational cycles
+    - Gradual degradation patterns  
+    - Sudden anomalies
+    - Environmental influences
+    """
